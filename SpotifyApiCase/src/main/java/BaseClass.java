6856c83299a1435aa274bb458333bf23aff3e7fc
Mina Odabaş <61796541+minaodabas@users.noreply.github.com>
2021-04-21 10:33:00 +0300

    Delete SpotifyApiCase directory

--- SpotifyApiCase/src/main/java/BaseClass.java
+++
@@ -1,9 +0,0 @@
-import services.Artist;
-import services.Search;
-
-public class BaseClass {
-
-    Artist artist = new Artist();
-    Search search = new Search();
-
-}
--- SpotifyApiCase/src/main/java/services/PlayList.java
+++
@@ -1,8 +0,0 @@
-package services;
-
-import spec.RequestSpec;
-
-public class PlayList extends RequestSpec {
-    public PlayList() { super("https://api.spotify.com/v1");
-    }
-}
